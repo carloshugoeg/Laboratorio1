e4fc3e366927c8cf9b4ffbaa93aa4ea0b9f9995a
Carlos Hugo Escobar Gomez <chescobarg@correo.url.edu.gt>
2024-07-29 09:38:20 -0600

    funcion mostrar informacion

---
+++ Tienda/Tienda/Producto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tienda
+{
+    internal class Producto
+    {
+        public Producto(string id, string nombre, double precio, int inventario)
+        {
+            Id = id;
+            Nombre = nombre;
+            Precio = precio;
+            InventarioStock = inventario;
+        }
+
+        private string Id {  get; set; }
+        private string Nombre { get; set; }
+        private double Precio { get; set; }
+        private int InventarioStock { get; set; }
+        private int Ventas { get; set; }
+
+        public void MostrarInformacion()
+        {
+            Console.Clear();
+            Console.WriteLine("-----INFORMACION-----");
+            Console.WriteLine("Id:" + Id);
+            Console.WriteLine("Nombre: "+ Nombre);
+            Console.WriteLine("Precio: Q." + Precio);
+            Console.WriteLine("Inventario: " + InventarioStock);
+            Console.WriteLine("Cantidad de ventas" + Ventas);
+            Console.WriteLine("\n Presione ENTER para regresar");
+            Console.ReadLine();
+        }
+    }
+}
--- Tienda/Tienda/Program.cs
+++ Tienda/Tienda/Program.cs
@@ -4,13 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TIENDA");
-            Console.WriteLine("    1. Ingresar Producto");
-            Console.WriteLine("    2. Consultar informacion de procucto");
-            Console.WriteLine("    3. Venta un producto");
-            Console.WriteLine("    4. Reabastecer producto");
-            Console.WriteLine("    5. Actualizar precio producto");
-            Console.WriteLine("    6. Mostrar Resumen de producto");
+
+            do
+            {
+                Console.WriteLine("---------TIENDA------------\n");
+                Console.WriteLine("    1. Ingresar Producto");
+                Console.WriteLine("    2. Consultar informacion de procucto");
+                Console.WriteLine("    3. Venta un producto");
+                Console.WriteLine("    4. Reabastecer producto");
+                Console.WriteLine("    5. Actualizar precio producto");
+                Console.WriteLine("    6. Mostrar Resumen de producto");
+                string opcion = Console.ReadLine();
+                switch(opcion) {
+                    case "1":
+
+            }while (true);
+            
+
         }
     }
 }
