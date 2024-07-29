9e2e61164c3908102c642ebf0ad7bbc5796bcfe8
Carlos Hugo Escobar Gomez <chescobarg@correo.url.edu.gt>
2024-07-29 09:27:37 -0600

    Creacion del menu principal del proyecto

---
+++ Tienda/Tienda/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tienda
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TIENDA");
+            Console.WriteLine("    1. Ingresar Producto");
+            Console.WriteLine("    2. Consultar informacion de procucto");
+            Console.WriteLine("    3. Venta un producto");
+            Console.WriteLine("    4. Reabastecer producto");
+            Console.WriteLine("    5. Actualizar precio producto");
+            Console.WriteLine("    6. Mostrar Resumen de producto");
+        }
+    }
+}
