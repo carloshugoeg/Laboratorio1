e6381f27e84fb27965b25e03d0a326e84a2aeb0c
Carlos Hugo Escobar Gomez <chescobarg@correo.url.edu.gt>
2024-07-29 09:03:20 -0600

    cambio excepciones

--- Laboratorio1/Program.cs
+++ Laboratorio1/Program.cs
@@ -28,6 +28,7 @@ static void MenuVenta()
 {
     List<string> productosList = new List<string>();
     List<double> preciosList = new List<double>();
+    string producto;
     do
     {
         try
@@ -35,7 +36,7 @@ static void MenuVenta()
             Console.Clear();
             Console.WriteLine("----NUEVA VENTA----");
             Console.Write("Ingrese nuevo producto: ");
-            string producto = Console.ReadLine();
+            producto = Console.ReadLine();
             if (producto.Length > 0)
             {
                 break;
@@ -44,9 +45,7 @@ static void MenuVenta()
             {
                 Console.WriteLine("El producto no puede ser nulo, intentelo de nuevo");
                 Console.ReadLine();
-            }
-              
-            
+            }            
         }
         catch(Exception ex)
         {
@@ -54,6 +53,53 @@ static void MenuVenta()
             Console.ReadLine();
             Console.Clear();
         }
+            
     }while (true);
+    productosList.Add(producto);
 
+
+}
+double PedirPrecio()
+{
+    do
+    {
+        try
+        {
+            double precio;
+            do
+            {
+                Console.WriteLine("Ingrese el precio de su producto: ");
+                precio = double.Parse(Console.ReadLine());
+                if (precio == 0)
+                {
+                    Console.WriteLine("Number cannot be 0. Please try again");
+                    Console.ReadLine();
+                }
+            } while (precio == 0);
+            return precio;
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("INPUT INVALIDO");
+            Console.WriteLine(ex.Message);
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("-----Nueva Venta-----");
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine("EL numero es demasiado grande");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("-----Nueva Venta-----");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("ERROR");
+            Console.WriteLine(ex.Message);
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("-----Nueva Venta-----");
+        }
+    } while (true);
 }
