320114003a977dcc31b1e4561494c0d442a3f4ac
Carlos Hugo Escobar Gomez <chescobarg@correo.url.edu.gt>
2024-07-29 08:48:52 -0600

    cambio1

--- Laboratorio1/Program.cs
+++ Laboratorio1/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Prueba");
+﻿do
+{
+    try
+    {
+        Console.Clear();
+        Console.WriteLine("Menu");
+        Console.WriteLine("     1. Ingresar Venta");
+        Console.WriteLine("     2. Salir");
+        string option = Console.ReadLine();
+        switch (option)
+        {
+            case "1":
+                MenuVenta();
+                break;
+
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        Console.ReadLine();
+        Console.Clear();
+    }
+
+} while (true);
+
+static void MenuVenta()
+{
+    List<string> productosList = new List<string>();
+    List<double> preciosList = new List<double>();
+    Console.Clear();
+    Console.WriteLine("----NUEVA VENTA----");
+
+    Console.WriteLine("Ingrese nuevo producto: ");
+
+}
