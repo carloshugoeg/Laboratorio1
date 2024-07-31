7b33291dd88743ea6785ad0524f8308b3810163b
Carlos Hugo Escobar Gomez <chescobarg@correo.url.edu.gt>
2024-07-31 07:23:43 -0600

    Debido al bajón de energía se enviaron todos los cambios hasta hoy. Se hizo el Program.Main en donde solamente está el menu del proyecto y todas las clases se crearon en la clase producto

--- Tienda/Tienda/Producto.cs
+++ Tienda/Tienda/Producto.cs
@@ -3,25 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Tienda
 {
     internal class Producto
     {
-        public Producto(string id, string nombre, double precio, int inventario)
+        public Producto(int id, string nombre, double precio, int inventario)
         {
             Id = id;
             Nombre = nombre;
             Precio = precio;
             InventarioStock = inventario;
+            Ventas = 0;
         }
 
-        private string Id {  get; set; }
+        private int Id {  get; set; }
         private string Nombre { get; set; }
         private double Precio { get; set; }
         private int InventarioStock { get; set; }
         private int Ventas { get; set; }
 
+
+       
+        public void VenderProducto()
+        {
+            int cantidadVender;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("-----VENDER PRODUCTO-----");
+                    Console.WriteLine("\nCantidad en stock: " + InventarioStock);
+                    
+                    Console.Write("\n\nCuantas unidades desea vender?: ");
+                    cantidadVender = int.Parse(Console.ReadLine());;
+                    if (cantidadVender != 0 && cantidadVender <= InventarioStock)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("No se puede ingresar cantidad 0 o nula");
+                    Console.ReadKey();
+                    Console.Clear();
+                    if (cantidadVender > InventarioStock) Console.WriteLine("La cantidad a vender excede a la cantidad en stock");
+                }
+                catch (FormatException ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Presione ENTER para continuar");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Presione ENTER para continuar");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                
+            } while (true);
+            Console.WriteLine("\nVenta realizada excitosamente!!");
+            Console.WriteLine($"Porfavor pagar Q.{cantidadVender * Precio} en ventanilla");
+            InventarioStock -= cantidadVender;
+            Ventas += cantidadVender;
+            Console.ReadKey();
+            Console.Clear();
+
+        }
         public void MostrarInformacion()
         {
             Console.Clear();
@@ -29,10 +80,93 @@ namespace Tienda
             Console.WriteLine("Id:" + Id);
             Console.WriteLine("Nombre: "+ Nombre);
             Console.WriteLine("Precio: Q." + Precio);
-            Console.WriteLine("Inventario: " + InventarioStock);
-            Console.WriteLine("Cantidad de ventas" + Ventas);
+            Console.WriteLine("Inventario:  " + InventarioStock);
             Console.WriteLine("\n Presione ENTER para regresar");
             Console.ReadLine();
         }
+        public Producto IngresarProducto(int id)
+        {
+            Producto producto;
+            Console.Clear();
+            Console.WriteLine("------Ingresar Producto------");
+            Console.WriteLine("ID: " + id);
+            Console.Write("\nIngrese Nombre: ");
+            string nombre = Console.ReadLine();
+            double precio = PedirPrecio();
+            int stock;
+            do
+            {
+                try
+                {
+                    Console.Write("Ingrese su cantidad en stock: ");
+                    stock = int.Parse(Console.ReadLine());
+                    if (stock != 0 && stock != null)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("No se puede ingresar cantidad 0 o nula");
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Presione ENTER para continuar");
+                    Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("------Ingresar Producto------");
+                }
+            } while (true);
+            producto = new Producto(id, nombre, precio, stock);
+            Console.WriteLine("\nProducto ingresado correctamente!!");
+            Console.WriteLine("Presione ENTER para continuar");
+            Console.ReadLine();
+            return producto;
+        }
+        public double PedirPrecio()
+        {
+            do
+            {
+                try
+                {
+
+                    double precio;
+                    do
+                    {
+                        Console.Write("Ingrese el precio de su producto: Q.");
+                        precio = double.Parse(Console.ReadLine());
+                        if (precio == 0 || precio == null)
+                        {
+                            Console.WriteLine("Valor no puede ser 0");
+                            Console.ReadLine();
+                        }
+                    } while (precio == 0);
+                    return precio;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("INPUT INVALIDO");
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("-----Nueva Venta-----");
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine("EL numero es demasiado grande");
+                    Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("-----Nueva Venta-----");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ERROR");
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("-----Nueva Venta-----");
+                }
+            } while (true);
+        }
+
     }
 }
--- Tienda/Tienda/Program.cs
+++ Tienda/Tienda/Program.cs
@@ -4,23 +4,49 @@
     {
         static void Main(string[] args)
         {
-
+            int id = 0001;
+            Producto producto = new Producto(0, "", 0.00, 0);
+            producto = producto.IngresarProducto(id);
+            id++;
             do
             {
+                Console.Clear();
                 Console.WriteLine("---------TIENDA------------\n");
                 Console.WriteLine("    1. Ingresar Producto");
-                Console.WriteLine("    2. Consultar informacion de procucto");
+                Console.WriteLine("    2. Consultar informacion de producto");
                 Console.WriteLine("    3. Venta un producto");
                 Console.WriteLine("    4. Reabastecer producto");
                 Console.WriteLine("    5. Actualizar precio producto");
                 Console.WriteLine("    6. Mostrar Resumen de producto");
+                Console.WriteLine("    7. Salir");
                 string opcion = Console.ReadLine();
-                switch(opcion) {
+                switch (opcion)
+                {
                     case "1":
+                        producto = producto.IngresarProducto(id);
+                        id += 1;
+                        break;
+                    case "2":
+                        producto.MostrarInformacion();
+                        break;
+                    case "3":
+                        producto.VenderProducto();
+                        break;
+                    case "4":
+                        producto.ReabastecerProducto();
+                        break;
+                    case "5":
+                        producto.ActualizarPrecio();
+                        break;
+                    case "6":
+                        producto.Resumen();
+                        break;
+                    case "7":
+                        Environment.Exit(0);
+                        break;
+                }
 
-            }while (true);
-            
-
+            } while (true);
         }
     }
 }
